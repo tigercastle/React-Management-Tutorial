3fae42b5bdf2105a1fb6268119cdbb040658e1b4
tigercastle <tigercastle@hanmail.net>
2021-07-30 01:55:46 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  render(){
+    <div className = "gray-background">
+      <img src={logo} lat="logo" />
+      <h2>Let's develop management system!</h2>
+    </div>
+  }
+
+
+}
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello React Project!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+export default App;
