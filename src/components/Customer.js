d0e8c65c9bde37c64eedded148bcd40128208c3b
tigercastle <tigercastle@hanmail.net>
2021-07-30 12:18:54 +0900

    Apply Material UI

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,22 +1,24 @@
+import { TableRow } from '@material-ui/core';
 import React from 'react';
-import CustomerInfo from './CustomerInfo';
-import CustomerProfile from './CustomerProfile';
+// import CustomerInfo from './CustomerInfo';
+// import CustomerProfile from './CustomerProfile';
+
+// import { Table } from '@material-ui/core/Table';
+// import { TableHead } from '@material-ui/core/TableHead';
+// import { TableBody } from '@material-ui/core/TableBody';
+import { TableCell } from '@material-ui/core';
 
 class Customer extends React.Component{
     render(){
         return (
-            <div>
-               <CustomerProfile 
-                 id={this.props.id}
-                 image={this.props.image}
-                 name={this.props.name}
-               />
-               <CustomerInfo
-                 birthday={this.props.birthday}
-                 gender={this.props.gender}
-                 job={this.props.job}
-               />
-            </div>
+               <TableRow>
+                    <TableCell>{this.props.id}</TableCell>
+                    <TableCell><img src={this.props.image} alt = "profile"/></TableCell>
+                    <TableCell>{this.props.name}</TableCell>
+                    <TableCell>{this.props.birthday}</TableCell>
+                    <TableCell>{this.props.gender}</TableCell>
+                    <TableCell>{this.props.job}</TableCell>
+               </TableRow>
         )
     }
 }
